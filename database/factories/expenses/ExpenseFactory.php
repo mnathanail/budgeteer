e69eae844966068989156291de8e085fe1cdbd7d
Manos Nathanail <m.nathanail@yahoo.gr>
2025-05-17 01:51:26 +0300

    Seeder and factory
    Related to: #8

---
+++ database/factories/expenses/ExpenseFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories\expenses;
+
+use App\Expense\Infrastructure\Eloquent\ExpenseEntity;
+use App\Shared\Domain\Uuid;
+use App\Shared\Domain\ValueObject\Cost;
+use App\Shared\Domain\ValueObject\Name;
+use DateTimeImmutable;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ExpenseEntity>
+ */
+class ExpenseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'uuid' => Uuid::fromString($this->faker->uuid()),
+            'name' => Name::fromString($this->faker->word()),
+            'type' => '',
+            'cost' => Cost::fromString((string)$this->faker->randomFloat(3)),
+            'date' => (new DateTimeImmutable())->format('d-m-Y'),
+        ];
+    }
+}
---
+++ database/seeders/expenses/ExpenseSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders\expenses;
+
+use Illuminate\Database\Seeder;
+
+class ExpenseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    }
+}
