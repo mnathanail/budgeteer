d49d73b5c1b615879f63ae8f7415c00054bc98b6
Manos Nathanail <m.nathanail@yahoo.gr>
2025-05-17 01:50:18 +0300

    Create Expense Domain
    Add Entity, VOs, Exception
    
    Related to: #8

---
+++ database/migrations/2025_05_13_140731_create_expenses_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expense', function (Blueprint $table) {
+            $table->uuid('expense_id')->nullable(false);
+            $table->string('name')->nullable(false);
+            $table->string('type')->nullable(false);
+            $table->string('cost')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
