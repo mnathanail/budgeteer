1ac5f760b782ec579219702df80bec493748266f
Manos Nathanail <m.nathanail@yahoo.gr>
2025-05-17 02:10:04 +0300

    fix regex
    Regex now checks if the number is positive. Add one more asserion
    Make assertion better
    
    Related to: #8

--- app/Shared/Domain/ValueObject/Cost.php
+++ app/Shared/Domain/ValueObject/Cost.php
@@ -9,13 +9,13 @@ final readonly class Cost
     public function __construct(
         private string $cost,
     ) {
-        Assertion::greaterThan($this->cost, 'Cost must be positive');
+        Assertion::greaterThan((float)$cost, 0, 'Cost cannot be negative');
     }
 
     public static function fromString(string $cost): self
     {
         Assertion::string($cost);
-        Assertion::regex($cost, '/^\d+(\.\d{1,3})?$/', 'Invalid cost format');
+        Assertion::regex($cost, '/^(?:[1-9]\d*|0)(?:\.\d{1,3})?$/', 'Invalid cost format');
 
         return new self($cost);
     }
