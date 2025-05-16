2a69381e004e9bcddbd34b787520a8a145d2e21c
Manos Nathanail <m.nathanail@yahoo.gr>
2025-05-17 02:15:24 +0300

    Fix wrong import
    Related to: #8

--- app/Expense/Domain/Exception/Exception.php
+++ app/Expense/Domain/Exception/Exception.php
@@ -3,8 +3,7 @@
 namespace App\Expense\Domain\Exception;
 
 use App\Shared\Domain\ExceptionInterface;
-use Psy\Exception\RuntimeException;
 
-final class Exception extends RuntimeException implements ExceptionInterface
+final class Exception extends \RuntimeException implements ExceptionInterface
 {
 }
