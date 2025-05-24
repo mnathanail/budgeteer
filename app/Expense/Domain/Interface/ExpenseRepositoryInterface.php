eb827bbab93bb71ae72e9a0f884e901b1d2cb78d
Manos Nathanail <m.nathanail@yahoo.gr>
2025-05-24 10:32:26 +0300

    Add UUID creation mechanism
    Add the getNextId method and the Interface + its implementation for it. The getNextId is going to create the uuid for the expenseId to be persisted
    
    Related to: #8

--- app/Expense/Domain/Interface/ExpenseRepositoryInterface.php
+++ app/Expense/Domain/Interface/ExpenseRepositoryInterface.php
@@ -3,8 +3,11 @@
 namespace App\Expense\Domain\Interface;
 
 use App\Expense\Domain\Entity\Expense;
+use App\Shared\Domain\ExpenseId;
 
 interface ExpenseRepositoryInterface
 {
     public function save(Expense $expense): void;
+
+    public function getNextId(): ExpenseId;
 }
