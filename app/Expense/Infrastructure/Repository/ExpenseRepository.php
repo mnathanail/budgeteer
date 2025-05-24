ee159646b57cf8421cfe95e9f59efb1fba7db4e3
Manos Nathanail <m.nathanail@yahoo.gr>
2025-05-24 23:28:32 +0300

    Add unimplemented method
    Implement getNextId
    
    Related to: #8

--- app/Expense/Infrastructure/Repository/ExpenseRepository.php
+++ app/Expense/Infrastructure/Repository/ExpenseRepository.php
@@ -5,19 +5,28 @@ namespace App\Expense\Infrastructure\Repository;
 use App\Expense\Domain\Entity\Expense;
 use App\Expense\Domain\Interface\ExpenseRepositoryInterface;
 use App\Expense\Infrastructure\Eloquent\ExpenseEntity;
+use App\Shared\Application\Uuid\UuidRepositoryInterface;
+use App\Shared\Domain\ExpenseId;
 use Illuminate\Database\Eloquent\Builder;
 
 class ExpenseRepository implements ExpenseRepositoryInterface
 {
-    private Builder $builder;
-
-    public function __construct()
-    {
+    public function __construct(
+        private readonly UuidRepositoryInterface $uuidRepository,
+        private Builder $builder
+    ) {
         $this->builder = ExpenseEntity::query();
     }
 
     public function save(Expense $expense): void
     {
         $this->builder->create($expense->toDatabase());
+    }
+
+    public function getNextId(): ExpenseId
+    {
+        return ExpenseId::fromString(
+            $this->uuidRepository->getUuid()->toString()
+        );
     }
 }
