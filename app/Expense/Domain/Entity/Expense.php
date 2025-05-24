08d5d25f860462eb4241ef731ef88e9b5fd1f2b3
Manos Nathanail <m.nathanail@yahoo.gr>
2025-05-24 10:18:59 +0300

    Update the Expense Entity
    add expense_id, make date format const, add some assertions for id, date, rename price to cost. Handle the date correctly
    
    Related to: #8

--- app/Expense/Domain/Entity/Expense.php
+++ app/Expense/Domain/Entity/Expense.php
@@ -3,28 +3,28 @@
 namespace App\Expense\Domain\Entity;
 
 use App\Expense\Domain\Enum\ExpenseType;
+use App\Shared\Domain\ExpenseId;
 use App\Shared\Domain\ValueObject\Cost;
 use App\Shared\Domain\ValueObject\Name;
 use Assert\Assertion;
 
 class Expense
 {
-    private Name $name;
-    private ExpenseType $type;
-    private Cost $cost;
-    private \DateTimeImmutable $date;
 
+    private const DATE_FORMAT = 'd-m-Y';
     public function __construct(
-        Name               $name,
-        ExpenseType        $type,
-        Cost               $cost,
-        \DateTimeImmutable $date
+        private ExpenseId $expenseId,
+        private Name $name,
+        private ExpenseType $type,
+        private Cost $cost,
+        private \DateTimeImmutable $date
     )
     {
-        $this->name = $name;
-        $this->type = $type;
-        $this->cost = $cost;
-        $this->date = $date;
+    }
+
+    public function getExpenseId(): ExpenseId
+    {
+        return $this->expenseId;
     }
 
     public function getName(): Name
@@ -50,29 +50,36 @@ class Expense
     public function toDatabase(): array
     {
         return [
+            'expense_id' => $this->getExpenseId()->toString(),
             'name' => $this->getName()->toString(),
             'type' => $this->getType()->value,
             'cost' => $this->getCost()->toString(),
-            'date' => $this->getDate()->format('d-m-Y'),
+            'date' => $this->getDate()->format(self::DATE_FORMAT),
         ];
     }
 
     public static function fromDatabase(array $row): Expense
     {
+        Assertion::keyExists($row, 'expense_id');
+        Assertion::uuid($row['expense_id']);
         Assertion::keyExists($row, 'name');
         Assertion::notEmpty($row['name']);
         Assertion::keyExists($row, 'type');
         Assertion::notEmpty($row['type']);
-        Assertion::keyExists($row, 'price');
-        Assertion::notEmpty($row['price']);
+        Assertion::keyExists($row, 'cost');
+        Assertion::notEmpty($row['cost']);
         Assertion::keyExists($row, 'date');
         Assertion::notEmpty($row['date']);
 
+        $date = \DateTimeImmutable::createFromFormat(self::DATE_FORMAT, $row['date']);
+        Assertion::isInstanceOf($date, \DateTimeImmutable::class);
+
         return new self(
+            ExpenseId::fromString($row['expense_id']),
             Name::fromString($row['name']),
             ExpenseType::from($row['type']),
-            Cost::fromString($row['price']),
-            new \DateTimeImmutable($row['date'])
+            Cost::fromString($row['cost']),
+            $date
         );
     }
 }
